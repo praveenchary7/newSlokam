1ddb3231d2a5fe9735f7857f9feb850e1fb9a208
praveen <pchary59@gmail.com>
2023-06-07 10:30:10 +0530

    hii everyone

---
+++ src/newsolkam/Test.java
@@ -0,0 +1,17 @@
+package newsolkam;
+
+public class Test {
+	
+	public void m1()
+	{
+		System.out.println("hello world");
+	}
+
+	public static void main(String[] args) {
+		Test t = new Test();
+		t.m1();
+		
+
+	}
+
+}
